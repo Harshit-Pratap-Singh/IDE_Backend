3edf92e36e5366015d4874e38885168bd1118dd6
Harshit Pratap Singh <singhharshitpratap@gmail.com>
2022-04-04 16:57:12 +0530

    fix 3

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ var corsOptions = {
   origin: "*",
   credentials: true,
 };
-
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); //middleware to parse the json body part
